9a059845f9faad7c5f71f342a7bf76d74b0f67e3
Alexander <Alexander.Jonstrup@outlook.com>
2023-08-28 13:46:41 +0200

    Project initialized.

---
+++ src/test/java/experis/filmapi/FilmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package experis.filmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
